ce9f256fda9a8dd0249c6997178b78066e620c78
Adri <amergam1012@g.educaand.es>
2023-02-15 12:27:58 +0100

    Revisión 1. amergam1012 - Entrega Examen

---
+++ unit-testing-using-nunit/PrimeService/PrimeService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace Prime.Services
+{
+    public class PrimeService
+    {
+        public bool IsPrime(int candidate)
+        {
+
+            if (candidate == 1) {
+                return false;
+            } 
+
+            for (int i = 2; i < candidate; i++)
+            {
+                if((candidate % i) == 0)
+                {
+                    return false;
+                }       
+            }
+
+            return true;
+        }
+    }
+}
